03a4899ee3b94616a04a917f099c943e7a6bef9b
Nilu89 <you@example.com>
2023-05-31 11:58:25 -0400

    Classes updated

--- src/o_o_programming/ch.9.classes.py
+++ src/o_o_programming/ch.9.classes.py
@@ -22,7 +22,35 @@ class Restaurant:
     new_restaurant.open_rest()
 
 
-print(f"9.3 Users")
+print(f'\n9.2 Three restaurants')
+
+
+class Restaurant:
+    pass
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def describe_rest(self):
+        print(f"The name of the restaurant is {self.name}.")
+        print(f"This restaurant's cuisine_type is {self.type}.")
+
+    def open_rest(self):
+        print(f"{self.name} is open from 8am to 10pm.")
+
+
+class Restaurant:
+    uzbekistan = Restaurant("Tandir", "Uzbek")
+    greece = Restaurant("Athens", "Greek")
+    uzbekistan.describe_rest()
+    uzbekistan.open_rest()
+    greece.describe_rest()
+    greece.open_rest()
+
+
+print(f"\n9.3 Users")
+
 
 class User:
     pass
@@ -38,6 +66,14 @@ class User:
         print(f"Hello {self.first_name}!")
         print(f"Nice to meet you!")
 
+
+class User:
     name = User('Lily', 'Shodieva')
     name.describe_user()
     name.greet_user()
+
+
+print('********************')
+
+
+
